676f89301d750cce0c1d8ac6b12fb760fb7e645e
jkim441 <jkim441@csc303x24.ad.calpoly.edu>
2019-04-03 14:41:02 -0700

    Planets are aligned

--- planets.py
+++ planets.py
@@ -1,7 +1,9 @@
 def weight_on_planets():
-   # write your code here
-   
-   
+    weight = int(input("What do you weigh on earth? "))
+    mars_weight = weight * 0.38
+    jupiter_weight = weight * 2.34
+    print("\nOn Mars you would weigh", mars_weight,"pounds.\nOn Jupiter you would weigh", jupiter_weight,"pounds.")
+ 
    
 if __name__ == '__main__':
    weight_on_planets()
